3c74bd8b718135cfc9386c294dab12afbbb6145f
rjhayy001 <101618965+rjhayy001@users.noreply.github.com>
2023-03-06 10:42:16 +0700

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 import './dotenv';
 
-export const client = new GraphQLClient(process.env.HASURA_HTTP_URL, {
+export const client:any = new GraphQLClient(process.env.HASURA_HTTP_URL, {
   headers: { "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET },
 });
