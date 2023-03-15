816bd2b691523b0110fb794a21e3590e404595a9
Ryan Joshua Charita <ryanjoshuacharita@Ryans-Mac-mini.local>
2023-03-15 13:21:19 +0800

    add cors

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import './src/dotenv';
 import auth from './src';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Parse JSON in request bodies
 app.use(express.json());
+app.use(cors());
 app.use('/auth', auth);
 
 app.listen(port, () => {
