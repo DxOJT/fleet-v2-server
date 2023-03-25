20deac6ef0693c39be3e949141b6500d0e5f05b6
root <root@ip-172-31-29-13.ap-southeast-1.compute.internal>
2023-03-25 08:29:15 +0000

    server

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import './src/dotenv';
 import auth from './src';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Parse JSON in request bodies
+app.use(cors());
 app.use(express.json());
 app.use('/auth', auth);
 
